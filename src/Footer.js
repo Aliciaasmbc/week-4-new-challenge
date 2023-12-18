d9cf4cc02decbef7da52c2a003bb4391c3805b7e
Aliciaasmbc <alicia.apreala@gmail.com>
2023-12-18 10:29:30 +0000

    created react app and imported existing project

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./Footer.css";
+
+export default function footer() {
+  return (
+    <p>
+      This project is{" "}
+      <a
+        href="https://github.com/Aliciaasmbc/week-4-challenge-react"
+        target="_blank"
+      >
+        Open-sourced on Github
+      </a>
+    </p>
+  );
+}

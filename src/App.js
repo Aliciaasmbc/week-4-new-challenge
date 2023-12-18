2c842182ba7ecbf0f497a9d75f2a89b567cff13a
Aliciaasmbc <alicia.apreala@gmail.com>
2023-12-18 13:24:04 +0000

    added axios and dummy search function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 
 import Weather from "./Weather";
 import Footer from "./Footer";
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Weather />
+
         <Footer />
       </header>
     </div>
